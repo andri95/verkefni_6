4a22a8299bafdf022c91f18aa51ccbc5cfc443f4
andri95 <afannar@gmail.com>
2018-11-06 18:33:50 +0000

    laga3

--- app.py
+++ app.py
@@ -13,18 +13,3 @@ def index():
     return "Hallo Heimur í Heroku og Github"
 
 bottle.run(host='0.0.0.0', port=argv[1])
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from sys import argv
-
-import bottle
-from bottle import *
-
-bottle.debug(True)
-
-@get('/')
-def index():
-    return "Hallo Heimur í Heroku og Github"
-
-bottle.run(host='0.0.0.0', port=argv[1])
