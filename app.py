6121ea30f3743c342be85b5736d965e87cef1872
andri95 <afannar@gmail.com>
2018-11-07 00:39:40 +0000

    smávægilegar breytingar

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def index():
     mynd6 = 'turn6.jpg'
     return template('index.tpl', mynd1 = mynd1, mynd2 = mynd2, mynd3 = mynd3, mynd4 = mynd4, mynd5 = mynd5, mynd6 = mynd6)
 
-@route('/static/<filename:re:.*\jpg>')
+@route('/static/<filename>')
 def send_image(filename):
     return static_file(filename, root='static')
 
