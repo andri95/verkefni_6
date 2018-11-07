96891af4a8d45b97a0d6e9ddc0d7badaff506212
andri95 <afannar@gmail.com>
2018-11-07 04:53:48 +0000

    prufa

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def skodad(mynd):
         valin_vara = request.get_cookie("vara")
         vorur.append(valin_vara)
 
-        return template('skodad.tpl', vorur = vorur)
+        return valin_vara
     else:
         return "þetta virkar ekki"
 
